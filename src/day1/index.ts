476c69596073d1bacdb911be7ccaef7ce96c83f3
Rene Borner <rene.borner@detim.de>
2020-12-01 10:23:25 +0100

    updated helper functions

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -1,12 +1,12 @@
-import {readFileToArray} from "../helper";
+import {readFileToNumberArray} from "../helper";
 
-const input = readFileToArray(1);
+const input = readFileToNumberArray(1);
 input.forEach(number1 => {
     input.forEach(number2 => {
         input.forEach(number3 => {
             // #bruteforce
-            if (+number1 + +number2 + +number3 === 2020) {
-                console.log(+number1 * +number2 * +number3);
+            if (number1 + number2 + number3 === 2020) {
+                console.log(number1 * number2 * number3);
             }
         })
     })
--- src/helper.ts
+++ src/helper.ts
@@ -1,5 +1,14 @@
 import * as fs from 'fs';
 
-export function readFileToArray(day: number, fileName = 'input') {
-    return fs.readFileSync(`/home/rene/Workspace/AdventOfCode2020/src/day${day}/${fileName}`).toString().split("\n");
+export function readFileToNumberArray(day: number, fileName = 'input') {
+    return fs.readFileSync(`/home/rene/Workspace/AdventOfCode2020/src/day${day}/${fileName}`)
+        .toString()
+        .split("\n")
+        .map(x => +x);
+}
+
+export function readFileToStringArray(day: number, fileName = 'input') {
+    return fs.readFileSync(`/home/rene/Workspace/AdventOfCode2020/src/day${day}/${fileName}`)
+        .toString()
+        .split("\n")
 }
